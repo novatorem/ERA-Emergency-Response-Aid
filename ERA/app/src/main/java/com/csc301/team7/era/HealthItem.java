aa5e3c4c79f86e776e7373d64e4d1ee521ea5982
Spasimir Vasilev <Spas@user3-233-25.wireless.utoronto.ca>
2017-03-15 15:03:59 -0400

    [#141492961] added the apimedic api functionality

---
+++ ERA/app/src/main/java/com/csc301/team7/era/HealthItem.java
@@ -0,0 +1,14 @@
+package com.csc301.team7.era;
+
+public class HealthItem
+{
+	/// <summary>
+    /// Item ID
+    /// </summary>
+    public int ID;
+    
+    /// <summary>
+    /// Item name
+    /// </summary>
+    public String Name;
+}
---
+++ ERA/app/src/main/java/com/csc301/team7/era/MatchedSpecialisation.java
@@ -0,0 +1,10 @@
+package com.csc301.team7.era;
+
+public class MatchedSpecialisation extends HealthItem{
+	
+	/// <summary>
+    /// ID of specialisation
+    /// </summary>
+	public Integer SpecialistID;
+	
+}
