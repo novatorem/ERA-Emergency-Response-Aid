a3a8f98ef4cdce86d33fe566a747cd7034700f2a
Abdulla Bin Haji <abdulla.binhaji@mail.utoronto.ca>
2017-03-15 20:19:26 -0400

    Update MedicalPage.java

--- ERA/app/src/main/java/com/csc301/team7/era/MedicalPage.java
+++ ERA/app/src/main/java/com/csc301/team7/era/MedicalPage.java
@@ -3,6 +3,9 @@ package com.csc301.team7.era;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 public class MedicalPage extends AppCompatActivity {
@@ -10,5 +13,14 @@ public class MedicalPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medical_page);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu); //your file name
+        return super.onCreateOptionsMenu(menu);
     }
 }
