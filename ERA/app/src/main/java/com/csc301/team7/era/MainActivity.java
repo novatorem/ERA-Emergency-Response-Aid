51d84b730441d6463abec28a044bc79d4d9942e6
Abdulla Bin Haji <abdulla.binhaji@mail.utoronto.ca>
2017-03-15 20:19:01 -0400

    Update MainActivity.java

--- ERA/app/src/main/java/com/csc301/team7/era/MainActivity.java
+++ ERA/app/src/main/java/com/csc301/team7/era/MainActivity.java
@@ -3,13 +3,20 @@ package com.csc301.team7.era;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 
     public void gotoMedicalPage(View view) {
@@ -17,5 +24,10 @@ public class MainActivity extends AppCompatActivity {
         startActivity(medicalPage);
     }
 
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu); //your file name
+        return super.onCreateOptionsMenu(menu);
+    }
 }
