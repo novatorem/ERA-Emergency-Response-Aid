5c355a4b6d06a858a314162e1284941ad3a09265
Aun Sheikh <aun.m.sheikh@gmail.com>
2017-03-30 18:52:46 -0400

    added intro slider for user tips

---
+++ ERA/app/src/main/java/com/csc301/team7/era/IntroActivity.java
@@ -0,0 +1,17 @@
+package com.csc301.team7.era;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.github.paolorotolo.appintro.AppIntro;
+
+public class IntroActivity extends AppIntro {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        //creating the intro tips slide
+
+    }
+}
